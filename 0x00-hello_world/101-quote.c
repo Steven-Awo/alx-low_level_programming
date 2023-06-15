358fd22162b80de5016b877204bace06e733ec33
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-15 22:39:25 +0100

    8 fix commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-#include<unistd.h>
 /**
  * main - A program that uses function to prints that is not printf or put
  *
- *Return: 0 (Success)
+ *Return: 1 (Success)
  */
 int main(void)
 {
-	write(2,
-	"\"and that piece of art is useful" "- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

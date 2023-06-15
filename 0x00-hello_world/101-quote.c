7231c111df33a1731012503d6b1249b826a6cf28
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-15 23:15:37 +0100

    8 fix commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<>
+#include<unistd.h>
 /**
  * main - A program that uses function to prints that is not printf or put
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

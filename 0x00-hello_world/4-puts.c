dae3dff9b8ff6c02344a3e10dae0380ee97d605f
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-15 18:15:06 +0100

    4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#iclude<stdio.h>
+/**
+ * main - Entry point
+ *
+ *Recent: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\Programming is like buillding a multilingual puzzle");
+	return (0);
+}

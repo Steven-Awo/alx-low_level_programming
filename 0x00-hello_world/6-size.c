1173d26574806f057ef8be07364ab9f2b677b141
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-15 20:12:17 +0100

    6 fix commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - Aprogram that uses the prinf function to print the sizes of fnctions
  *
- *Recent: Always 0 (Success)
+ *Return: 0 (Success)
  */
 int main(void)
 {

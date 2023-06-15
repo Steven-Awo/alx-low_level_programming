729ec2b6df363e48d3d542f181fc68b8f3f14635
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-15 19:51:01 +0100

    5 fix commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - A program that uses printf function to print a statement
  *
- *Recent: Always 0 (Success)
+ *Recent: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }

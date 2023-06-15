9d20733afebc6f24895d4c982a243acbedf78369
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-15 18:18:11 +0100

    5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#iclude<stdio.h>
+/**
+ * main - Entry point
+ *
+ *Recent: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

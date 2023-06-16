c1362913c7cc817d10b360c5a620afa246bc78d4
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 18:09:45 +0100

    7. Smile in the mirror fix commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - A program that prints lowercase alphabet in reverserve
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+	char x;
+
+	for (x = 'z'; x <= 'a'; x--)
+	putchar(x);
+	putchar('\n');
+	return (0);
+
+}

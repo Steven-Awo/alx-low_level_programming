dc5f01fc8e04391eb2816f076043ed080748bcae
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 14:08:32 +0100

    22. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet fix commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; c++)
+	putchar(c, "\n");
+	return (0);
+}

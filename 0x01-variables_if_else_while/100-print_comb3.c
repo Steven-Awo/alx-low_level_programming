1a09a2948a004506b6756c94127755c895a5cdea
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 19:16:08 +0100

    9 & 100 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,18 @@ int main(void)
 
 	for (x = '0'; x <= '9'; x++)
 	{
-	putchar(x);
-	}
 	for (y = '0'; y <= '9'; y++)
-	putchar(y);
 	{
-	if (y != '9')
+	if (x != d)
+	{
+	putchar(x);
+	putchar(y);
+	if (x == '9' && y == '9')
+	continue;
 	putchar(',');
 	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);

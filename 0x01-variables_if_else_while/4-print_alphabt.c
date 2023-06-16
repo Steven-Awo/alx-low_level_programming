99fa48971bc6b9fcfbee68601d4f5770f3415990
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 16:19:21 +0100

    4. When I was having that alphabet soup, I never thought that it would pay off fix commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - A program that prints alphabet in a new line(lowercase)
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+	char x;
+
+	for (x = 'a'; x = <= 'z'; x++)
+	{
+	if (x != 'e' || 'q')
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

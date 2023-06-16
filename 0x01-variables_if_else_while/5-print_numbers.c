2aa595ecb5e799d7ac562cff0252b7c680120c91
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 16:58:25 +0100

    6. Numberz fix commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - A program that prints single digit numbers of base 10
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+	putchar(x)
+	}
+	putchar('\n')
+	return (0);
+}

89c32899d3f980d171af762039996d55375a1a63
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 13:39:35 +0100

    1. The last digit done commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - A program check the last digit of a number
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+	printf(n, lastd, " Last digit of %d and %d and is greater than 5\n");
+	}
+	else if (lastd == 0)
+	{
+	printf(n, lastd, " Last digit of %d and %d and is 0\n");
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+	printf("Last digit of %d and %d is less than 6 and not 0\n", n, lastd);
+	}
+	return (0);
+
+}

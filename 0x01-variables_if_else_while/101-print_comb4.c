678cfda4daf191ecab16034d5ed16fb39d56ce23
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 19:31:56 +0100

    101 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - A program that prints lowercase & Uppercase alphabet
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+        int x, y;
+
+        for (x = '0'; x <= '9'; x++)
+        {
+	for (y = x + 1; y <= '9'; y++)
+        {
+        for (z = x + 1; z <= '9'; z++)
+        {
+        if ((x != y) != z)
+        {
+        putchar(x);
+        putchar(y);
+	putchar(z);
+        if (x == '7' && y == '8')
+        continue;
+        putchar(',');
+        putchar(' ');
+        }
+	}
+        }
+        }
+        putchar('\n');
+        return (0);
+
+}

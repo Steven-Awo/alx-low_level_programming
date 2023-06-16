3cdd6bde92e7959b6e4c8369bf2e959a486ae9d6
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-16 18:24:40 +0100

    hexadecimal 1-9 a-f commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints lowercase & Uppercase alphabet
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+	char x, y;
+
+	for (x = '0'; x <= '10'; x++)
+	putchar(x);
+	for (y = 'a'; y <= 'f'; y++)
+	putchar(y);
+	putchar('\n');
+	return (0);
+
+}

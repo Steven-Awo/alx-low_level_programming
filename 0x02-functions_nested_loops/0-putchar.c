eb868b44274315ef4563b5a04ec23755542803b5
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-20 13:53:48 +0100

    _PUCHAR COMMIT TASK 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * _putchar - A program that writes the character c on th stdout
  * @c: The character to print
  *
- *Return: 1 (Success)
+ *Return: 0n success 1.
  *On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 
 {
+	
 	return (write(1, &c, 1));
 }

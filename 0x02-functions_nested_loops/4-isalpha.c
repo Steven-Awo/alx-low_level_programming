53743e0219a2f3ef9934634ee9d1a2b666a92048
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-20 20:42:07 +0100

    Task 4 check for ALPHABETS commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,8 @@
 
 /**
  * _isalpha - A program that checks for alphabets
- *
- *Return: 1 (Success) or 0 else
+ *@c: To check the character c
+ * Return: 1 (Success) or 0 else
  */
 
 int _isalpha(int c)

0bf24f5de4dd19ca85d5cce452d68e0c251dfe77
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-20 21:49:38 +0100

    Task 6 computes the absolute value of an integer commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A program that prints lowercase & Uppercase alphabet 10x
+ * @c: the figure to be computed
+ * Return: value or zero (Success)
+ */
+
+int _abs(int c)
+
+{
+	
+	if (c < 0)
+	{
+	int absol_val;
+
+	absol_val = c * -1;
+	return (absol_val);
+	}
+	return (c);
+}

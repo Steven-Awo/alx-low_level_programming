7a81627d6ddfcb15a56b5b227c9a5c3cccd77d6c
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-20 18:05:40 +0100

    Task 1 lower case alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - A program that makes alphabet in lowercase using function
+ *
+ * Retur: void (success)
+ */
+
+void print_alphabet(void)
+
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+_putchar(alph);
+_putchar('\n');
+return;
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - A program that prints alphabet in lowercase using function
+ *
+ * Retur: 0 (success)
+ */
+
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
+}

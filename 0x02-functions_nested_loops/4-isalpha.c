21be61becf8a1df4fee25cea3cf4bbc9014253d9
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-20 20:34:26 +0100

    Task 4 check for ALPHABETS commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - A program that checks for alphabets
+ *
+ *Return: 1 (Success) or 0 else
+ */
+
+int _isalpha(int c)
+
+{
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}

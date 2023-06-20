010977c7941469c41d7b9212ff15731dd3e36b8d
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-20 19:48:20 +0100

    Task 2 lower case alphabet 10x commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - A program that prints lowercase & Uppercase alphabet 10x
+ * print_alphabet_x10 - A program that prints lowercase alphabet 10x
  *
  * Return: void (Success)
  */

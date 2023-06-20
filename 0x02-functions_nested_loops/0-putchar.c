911c7d5cb3ce10c9d28a35be51445eb2dc1cd8f4
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-20 17:43:17 +0100

    _PUCHAR COMMIT TASK 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * main - A program that prints the word '_puchar'
- * is written by mbah
- * Return: 0 success
+ * main - A program that prints putchar with function
+ * Return: 0 (success)
  */
 
 int main(void)
+
 {
-_putchar('_')
-_putchar('p')
-_putchar('u')
-_putchar('t')
-_putchar('c')
-_putchar('h')
-_putchar('a')
-_putchar('r')
-_putchar('\n')
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }

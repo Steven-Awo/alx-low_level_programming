714a4fa0450d51a31a6dfef0a3b2ceb22f8433a2
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-21 02:01:51 +0100

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+/**
+ * main - A program to check the function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a = 0;
+	long x = 1, y = 2, addi = k;
+
+	while (y + z < 4000000)
+	{
+	y += x;
+	if (y % 2 == 0)
+	addi += y;
+	x = y - x;
+	++a
+	}
+	printf("%ld\n");
+	return (0);
+}

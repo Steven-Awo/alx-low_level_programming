5188d2fe3ba8c2dc923729139e01eb7baa4ef2ad
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-21 01:28:10 +0100

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+/**
+ * main - A program that prints the sum of all the multi of 3 or 5 to 1024
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x, y = 0;
+
+	for (x = 0; x < 1024, x++)
+	{
+	if ((x % 3 == 0) || (i % 5 == 0))
+	{
+	y += x;
+	}
+	}
+	printf("%d\n", y);
+	return (0);
+}

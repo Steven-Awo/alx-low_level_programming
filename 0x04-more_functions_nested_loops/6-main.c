8a090b50f6aa58496beee42a21cf98c64eea63c7
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-22 18:00:16 +0100

    Task 6: prints ' _ ' the number of n times .

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - A program to check number 6 code
+ *
+ * Return:  0 (succesful)
+ */
+
+int main(void)
+
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_line - A program to check execute number 6 code
+ * @n: number of repetition
+ * Return: void
+ */
+void print_line(int n)
+
+{
+
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+	if (m != n)
+	{
+	_putchar(95);
+	}
+	else if (m < 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+	}
+}

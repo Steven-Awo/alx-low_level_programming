6b27e2366cae9425a3e703c774edb8af8ec2289d
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-22 18:13:56 +0100

    Task 6: prints ' _ ' the number of n times .

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,15 +11,16 @@ void print_line(int n)
 
 	int m;
 
-	for (m = 0; m < n; m++)
-	{
-	if (m <= 0)
+	if (n <= 0)
 	{
 	_putchar('\n');
 	}
 	else
 	{
+	for (m = 0; m < n; m++)
+	{
 	_putchar(95);
 	}
+	_putchar('\n');
 	}
 }

c8e47e3c7f15205a5a6d945533b0088e267d4dfa
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-22 19:53:56 +0100

    Task 8: prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - A program that test code
+ * @size: The size function
+ * Return: 0 (successful)
+ */
+
+void print_square(int size)
+
+{
+	int a, b;
+
+	if (size < 0)
+	{
+	_putchar('\n');
+	}
+	else if (size == 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < size; a++)
+	{
+	for (b = 0; b < size; b++)
+	{
+	_putchar(35);
+	}
+	_putchar(35);
+	_putchar('\n');
+	}
+	}
+}

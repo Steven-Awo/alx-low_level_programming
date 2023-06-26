f867e6ceb19efc126e474fe6ba09a48649356ed3
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-26 14:21:07 +0100

    TASK 0: function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - A program that changes a pointer to an int
+ * @n: THE VARIABLE TO BE POINT to to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - A program to print single charactercheck
+ * @c: the character c  is repesenting is checked
+ * Return: 1 (successful)
+ * On error, -1 is returned, errno is set appropriately.
+ */
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}

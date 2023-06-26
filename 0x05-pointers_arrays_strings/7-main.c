5c990631930dff13a289de8730d72a5eb9a8012c
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-26 18:59:50 +0100

    TASK 6:  a function  that prints half of a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program used to check the code  
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - A program that prints a string a new line, to stdout
+ * @str: The caariable that is exercuted
+ * Return: void
+ */
+
+void puts_half(char *str)
+
+{
+	int x;
+	int y = 0;
+
+	while (str[y] != '\0')
+	{
+	y++;
+	}
+	if (y % 2 == 1)
+	{
+	x = (y - 1) / 2;
+	x += 1;
+	}
+	else
+	{
+	x = y / 2;
+	}
+	for (; x < y; x++)
+	_putchar(str[x]);
+	_putchar('\n');
+}

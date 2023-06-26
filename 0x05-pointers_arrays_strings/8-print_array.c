753bd98a10d67567b34aafce5149efb14279891f
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-26 19:31:48 +0100

    TASK 8:  a function  that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - A PROGRAM TO RETURN THE LENGTH OF A STRING
+ * @n: the ARRAY
+ * @a: the number of element in n
+ * Return: 0 (successful)
+ */
+
+void print_array(int *a, int n)
+
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+	printf("%d", a[x]);
+	printf(",");
+	printf(" ");
+	}
+	printf("\n");
+}

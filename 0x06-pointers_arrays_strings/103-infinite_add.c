55d51b00a7e4ed8e8773e59ace93605d3042e6a9
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-29 21:45:24 +0100

    TASK 11: a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,16 +1,38 @@
 #include "main.h"
 
 /**
- * print_buffer - A program to encoder rot13
- * @n1: the number of array
- * @n2: 
- * @r:
- * @size_r:
- * Return: *x (Successful)
+ * infinite_add - A program to encoder rot13
+ * @n1: the first value added
+ * @n2: the second value added
+ * @r: the storage elements in array
+ * @size_r: size of r
+ * Return: *r (Successful)
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 {
+	int a = 0;
+	int b = 0;
 
+	for (; *n1 && *n2; n1--, n2--, size_r--)
+	{
+	a = (*n1 - '0') + (*n2 - '0');
+	a += b;
+	*(r + size_r) = (a % 10) + '0';
+	b = a / 10;
+	}
+	for (; *n1; n1--, size_r++)
+	{
+	a = *(n1 - '0') + b;
+	*(r + size_r) = (a % 10) + '0';
+	b = a / 10;
+	}
+	for (; *n2; n2--, size_r--)
+	{
+	a = (*n2 - '0') + b;
+	*(r + size_r) = (a % 10) + '0';
+	b = a / 10;
+	}
+	return (r);
 }

3dbe75a029cd79d23c865905721f32d7fef0936a
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-29 18:59:17 +0100

    TASK 10: Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_buffer - A program to encoder rot13
+ * @n1: the number of array
+ * @n2: 
+ * @r:
+ * @size_r:
+ * Return: *x (Successful)
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+{
+
+}

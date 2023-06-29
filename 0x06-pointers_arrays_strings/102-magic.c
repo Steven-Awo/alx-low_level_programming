2872c5958826e0533d34e8b0f8332e2a0f6397ee
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-29 18:22:59 +0100

    TASK 10: Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that check the code.
+ *
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int x;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &x;
+ /*
+  * write your line of code here...
+  * Remember:
+  * - you are not allowed to use y
+  * - you are not allowed to modify z
+  * - only one statement
+  * -you are not allowed to code anything else than this line of code
+ */
+	*(p + 5) = 98;
+/* .....so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}

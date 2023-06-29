207ebcf64e9add54d162c0b87786fc0fd23c36f2
Steven-Awo <rotimiawomodu@gmail.com>
2023-06-29 14:38:43 +0100

    TASK 0: a function that copies a string..

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - A program to concatenate two strngs
+ * @src: The source of the file
+ * @dest: The destination of the file
+ * @n: the number of byte
+ * Return: 0 (Successful)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int ind = 0;
+	int src_l = 0;
+
+	while (src[ind++])
+	{
+	src_l++;
+	}
+	for (ind = 0; src[ind] && ind < n; ind++)
+	{
+	dest[ind] = src[ind];
+	}
+	for (ind = src_l; ind < n; ind++)
+	{
+	dest[ind] = '\0';
+	}
+	return (dest);
+}

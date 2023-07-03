4de0f152053dcbdecc2a317203417340534a8575
Steven-Awo <rotimiawomodu@gmail.com>
2023-07-03 18:27:46 +0100

    TAK 7: a function that sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - A program that searches a string for any of a set of bytes
+ * @s: The input
+ * @to: The input
+ * Return: 0 (Successful)
+ */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}

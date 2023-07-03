48f972af6b5f951dc99c1fdd064f1f68d10f2a6d
Steven-Awo <rotimiawomodu@gmail.com>
2023-07-03 18:05:19 +0100

    TAK 7: a function that prints the sum of the two diagonals of a square matrix of integers.

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - A program that gets the length of a prefix substring.
+ * @a: The array
+ * @size: The size
+ * Return: 0 (Successful)
+ */
+
+void print_diagsums(int *a, int size)
+
+{
+	int x, y;
+	int add1 = 0;
+	int add2 = 0;
+
+	for (x = 0; x <= (size * size); x = x + size + 1)
+	{
+	add1 = add1 + a[x];
+	}
+	for (x = size - 1; y <= (size * size) - size; y = y + size - 1)
+	{
+	add2 = add2 + a[y];
+	}
+	printf("%d, %d\n", add1, add2);
+}

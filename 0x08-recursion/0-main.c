c73bed452d1034bfbd1589e26a23c07e255eba01
Steven-Awo <rotimiawomodu@gmail.com>
2023-07-05 14:18:23 +0100

    TASK 0: a function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - A program that test the code
+ *
+ * Return: 0 (successful)
+ */
+
+int main(void)
+
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}

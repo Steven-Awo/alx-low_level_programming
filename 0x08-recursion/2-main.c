7820f1a063e064f016623ffe324e6591981a68d1
Steven-Awo <rotimiawomodu@gmail.com>
2023-07-05 15:08:37 +0100

    TASK 2: a function that returns the length of a string.

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that test the code
+ *
+ * Return: 0 (successful)
+ */
+
+
+int main(void)
+
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}

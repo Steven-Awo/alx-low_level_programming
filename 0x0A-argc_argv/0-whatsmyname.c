b883c9316b8ede296debdecfb660d6e90a2ce617
Steven-Awo <rotimiawomodu@gmail.com>
2023-07-07 16:21:17 +0100

    TASK 0 : a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: 0 (successfukl)
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

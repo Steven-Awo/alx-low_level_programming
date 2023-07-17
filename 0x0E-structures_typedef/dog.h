540d3136ff44b6179d4e7080cbfe9dfa8d2d4d1e
Steven-Awo <rotimiawomodu@gmail.com>
2023-07-17 17:52:30 +0100

    TASK 0: Define a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - The attributes of the dog
+ * @name: The dog's name.
+ * @age: The dog's name.
+ * @owner: The dog's owner.
+ * Description: The attributes of a dog.
+ */
+
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for the dog's structure
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif

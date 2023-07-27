37199f7de514839fb13b806bccf0efc28e20d856
Rotimi Awomodu <rotimiawomodu@gail.com>
2023-07-27 21:36:51 +0100

    TASK 3: A function that adds a new node at the end of a list_t list.

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_node_end - A program that adds to the end of a list a new node
+ * @head: The first linked list
+ * @str: The string to add to the node
+ * Return: hold and new list (successful) or NULL (unsuccessful)
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+
+{
+	list_t *nw_list, *hold;
+
+	if (str != NULL)
+	{
+	nw_list = malloc(sizeof(list_t));
+	if (nw_list == NULL)
+	{
+	return (NULL);
+	}
+	nw_list->str = strdup(str);
+	nw_list->len = _strlen(str);
+	nw_list->next = NULL;
+	if (*head == NULL)
+	{
+	*head  = nw_list;
+	return (*head);
+	}
+	else
+	{
+	hold = *head;
+	while (hold->next)
+	{
+	hold = hold->next;
+	}
+	hold->next = nw_list;
+	return (hold);
+	}}
+	return (NULL);
+}
+
+/**
+  * _strlen - A program that returns the length of a string
+  * @s: the string to be counted
+  * Return: The string's length
+  */
+
+int _strlen(const char *s)
+
+{
+	int x = 0;
+
+	while (*s)
+	{
+	s++;
+	x++;
+	}
+	return (x);
+}

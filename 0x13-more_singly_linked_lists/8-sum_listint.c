dc3e593a01962b16eadbbf5bb884e3d5fcc64446
Rotimi Awomodu <rotimiawomodu@gail.com>
2023-07-31 19:08:35 +0100

    TASK 6: a function that returns the sum of all the data (n) of a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - A program that adds all the data in
+ * a listint_t list
+ * @head: the first node in the linked list
+ * Return: add
+ */
+
+int sum_listint(listint_t *head)
+
+{
+	int addd = 0;
+	listint_t *hold = head;
+
+	while (hold)
+	{
+	addd += hold->n;
+	hold = hold->next;
+	}
+	return (addd);
+}
+

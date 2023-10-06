641489066fe5fac5aed94e0b19145cc1d10f796a
Steven-Awo <rotimiawomodu@gmail.com>
2023-10-06 23:32:02 +0100

    TASK 7: Rewrite the previous functions using these data structures

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -2,16 +2,15 @@
 
 shash_table_t *shash_table_create(unsigned long int size);
 
-void shash_table_delete(shash_table_t *ht);
-
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-
-void shash_table_print(const shash_table_t *ht);
 
 char *shash_table_get(const shash_table_t *ht, const char *key);
 
+void shash_table_print(const shash_table_t *ht);
+
 void shash_table_print_rev(const shash_table_t *ht);
 
+void shash_table_delete(shash_table_t *ht);
 
 /**
  * shash_table_create - A function that creates a sorted hash table.

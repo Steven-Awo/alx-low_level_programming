8b90b3c7b37ad35cd1460d3cb5df120928ce74a5
StevenAwo <rotimiawomodu@gmail.com>
2024-01-17 16:51:04 +0100

    TASK 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -11,38 +11,34 @@
  */
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
-
 {
-	size_t indx, a, b;
+	size_t index, a, b;
 	listint_t *prevus;
-
+	
 	if (list == NULL || size == 0)
 	{
 	return (NULL);
 	}
 	b = (size_t)sqrt((double)size);
-	indx = 0;
+	index = 0;
 	a = 0;
 	do {
 		prevus = list;
 		a++;
-		indx = a * b;
+		index = a * b;
 
-		while (list->next && list->indx < indx)
+		while (list->next && list->index < index)
 			list = list->next;
-		if (list->next == NULL && indx != list->indx)
-			indx = list->indx;
-		printf("Value checked at indx [%d] = [%d]\n",
-				(int)indx, list->n);
-
-	} while (indx < size && list->next && list->n < value);
+		if (list->next == NULL && index != list->index)
+			index = list->index;
+		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+	} while (index < size && list->next && list->n < value);
 	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)prevus->indx,
-			(int)list->indx);
-
-	for (; prevus && prevus->indx <= list->indx; prevus = prevus->next)
+	printf("[%d] and [%d]\n", (int)prevus->index, (int)list->index);
+	for (; prevus && prevus->index <= list->index; prevus = prevus->next)
 	{
-	printf("Value checked at indx [%d] = [%d]\n", (int)prevus->indx, prevus->n);
+	printf("Value checked at index [%d] = [%d]\n",
+			(int)prevus->index, prevus->n);
 	if (prevus->n == value)
 		return (prevus);
 	}
